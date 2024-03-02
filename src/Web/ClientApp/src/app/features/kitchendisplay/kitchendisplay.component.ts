1079e3dd7fc42a0e802fe67ca3298bc53eb46532
Andrey-Sabile <sabileandrey@gmail.com>
2024-03-02 16:05:31 +0800

    Remove getItemName function. Not needed anymore.

--- src/Web/ClientApp/src/app/features/kitchendisplay/kitchendisplay.component.ts
+++ src/Web/ClientApp/src/app/features/kitchendisplay/kitchendisplay.component.ts
@@ -8,14 +8,12 @@ import { OrderDto, OrdersClient, UpdateOrderCommand, MenuItem, MenuItemsClient }
   templateUrl: './kitchendisplay.component.html',
 })
 export class KitchendisplayComponent implements OnInit{
-  public orders: OrderDto[] = [];
   public remainingOrders: OrderDto[];
   public menuItems: MenuItem[] = [];
 
   constructor(
     private orderClient: OrdersClient,
     private menuItemsClient: MenuItemsClient,
-
   ){}
 
   ngOnInit(): void {
@@ -26,8 +24,8 @@ export class KitchendisplayComponent implements OnInit{
   getOrders(): void {
     this.orderClient.getOrdersUpdated().subscribe({
       next: result => {
-        this.orders = result
-        this.remainingOrders = this.orders.filter(order => !order.done)
+        this.remainingOrders = result;
+        this.remainingOrders = this.remainingOrders.filter(order => !order.done);
       },
       error: error => console.error(error),
     });
@@ -44,9 +42,5 @@ export class KitchendisplayComponent implements OnInit{
       next: result => this.menuItems = result,
       error : error => console.error(error)
     });
-  }
-
-  getItemName(id: number): string|undefined {
-    return this.menuItems.find(menuItem => menuItem.id === id)?.name
   }
 }
