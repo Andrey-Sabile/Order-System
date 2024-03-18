f58a803177945c6376d2616a32f2b966f0602c76
Andrey-Sabile <sabileandrey@gmail.com>
2024-03-18 19:14:02 +0800

    Add Sidenav component

--- src/Web/ClientApp/src/app/app.component.ts
+++ src/Web/ClientApp/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavMenuComponent } from './shared/components/nav-menu/nav-menu.component';
+import { SideNavComponent } from './shared/components/side-nav/side-nav.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     standalone: true,
-    imports: [NavMenuComponent, RouterOutlet]
+    imports: [NavMenuComponent, RouterOutlet, SideNavComponent]
 })
 export class AppComponent {
   title = 'app';
